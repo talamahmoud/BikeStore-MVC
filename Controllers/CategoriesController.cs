9de5890ef198502e3027eb1db61293aa39c80e86
talamahmoud <tala.ismail.kafa@gmail.com>
2024-05-20 04:58:11 +0300

    Create and Edit Category

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
 ﻿using BookStore.Data;
+using BookStore.Models;
+using BookStore.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
@@ -16,5 +18,43 @@ namespace BookStore.Controllers
             var categories = context.Categories.ToList();
             return View(categories);
         }
-    }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CategoryVM categoryVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", categoryVM);
+            }
+            var category = new Category
+            {
+                Name = categoryVM.Name
+        };
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return Redirect("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            //var category = context.Categories.Find(id);
+            return View("Create");
+        }
+        [HttpPost]
+		public IActionResult Edit(CategoryVM categoryVM)
+		{
+			var category = context.Categories.Find(categoryVM.Id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            category.Name = categoryVM.Name;
+            context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
 }
---
+++ ViewModel/CategoryVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.ViewModel
+{
+    public class CategoryVM
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage="Category name is req")]
+        [MaxLength(30)]
+        public string Name { get; set; } = null!;
+
+    }
+}
